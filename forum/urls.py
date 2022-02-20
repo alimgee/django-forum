d4c8615755240687c7dff9cfdad878e53585272a
alanmcgee <mcgee.al@gmail.com>
2022-02-20 21:01:36 +0000

    Adding route for logout and add requirements file

--- forum/urls.py
+++ forum/urls.py
@@ -13,15 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path
 from boards import views
 from accounts import views as account_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
-    path('signup/', account_views.signup, name="signup")
+    path('signup/', account_views.signup, name="signup"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout")
 ]
