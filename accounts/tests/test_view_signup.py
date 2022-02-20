1c59fd1ab9fd5f4e0f413f2fb093f6d3aa832ea2
alanmcgee <mcgee.al@gmail.com>
2022-02-20 12:39:32 +0000

    Reorganising tests in accounts app

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -1,8 +1,8 @@
 from django.shortcuts import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import signup
-from .forms import SignUpForm
+from ..views import signup
+from ..forms import SignUpForm
 from django.contrib.auth.models import User
 
 class SignUpTests(TestCase):
@@ -23,6 +23,16 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, SignUpForm)
+    
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 class SuccessfulSignUpTests(TestCase):
