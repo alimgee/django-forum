defba70da1b9b0b2b8a56afd70329f9fb1fbebf7
alanmcgee <mcgee.al@gmail.com>
2022-02-26 15:45:15 +0000

    Adding password reset functionality

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -1,9 +1,10 @@
-from django.shortcuts import reverse
-from django.urls import resolve
+from django.contrib.auth.models import User
+from django.urls import resolve, reverse
 from django.test import TestCase
+
 from ..views import signup
 from ..forms import SignUpForm
-from django.contrib.auth.models import User
+
 
 class SignUpTests(TestCase):
     def setUp(self):
@@ -23,11 +24,10 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, SignUpForm)
-    
+
     def test_form_inputs(self):
         '''
-        The view must contain five inputs: csrf, username, email,
-        password1, password2
+        The view must contain five inputs: csrf, username, email, password1, password2
         '''
         self.assertContains(self.response, '<input', 5)
         self.assertContains(self.response, 'type="text"', 1)
@@ -40,7 +40,7 @@ class SuccessfulSignUpTests(TestCase):
         url = reverse('signup')
         data = {
             'username': 'john',
-            'email': 'test@test.com',
+            'email': 'john@doe.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456'
         }
@@ -59,12 +59,12 @@ class SuccessfulSignUpTests(TestCase):
     def test_user_authentication(self):
         '''
         Create a new request to an arbitrary page.
-        The resulting response should now have a `user` to its context,
-        after a successful sign up.
+        The resulting response should now have an `user` to its context, after a successful sign up.
         '''
         response = self.client.get(self.home_url)
         user = response.context.get('user')
         self.assertTrue(user.is_authenticated)
+
 
 class InvalidSignUpTests(TestCase):
     def setUp(self):
@@ -83,10 +83,3 @@ class InvalidSignUpTests(TestCase):
 
     def test_dont_create_user(self):
         self.assertFalse(User.objects.exists())
-
-class SignUpFormTest(TestCase):
-    def test_form_has_fields(self):
-        form = SignUpForm()
-        expected = ['username', 'email', 'password1', 'password2',]
-        actual = list(form.fields)
-        self.assertSequenceEqual(expected, actual)
